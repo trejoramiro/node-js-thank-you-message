61ca35ef3b993e04ec9c5192d714276f50c14ee6
Ramiro Trejo <trejoramiro1@gmail.com>
2017-01-11 16:31:46 -0600

    Add <br>

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addComment(comment) {
 function createCommentString() {
 	var commentListString = '<ul>';
 	for(var i = 0; i < comments.length; i++) {
-			commentListString += '<li>' + comments[i]['message'] + '<br><i>' +  comments[i]['time'] + '</i> </li>';
+			commentListString += '<li>' + comments[i]['message'] + '<br><i>' +  comments[i]['time'] + '</i> </li><br>';
 	}
 	commentListString += '</ul><br>';
 	return commentListString;
@@ -34,7 +34,7 @@ http.createServer(function (request, response) {
 	console.log(querystring.parse(request.url));
 	timesVisited++;
 	var commentsString = createCommentString()
-	var commentForm = commentsString + '<form action=""> Comment: <br> <input type="text" name="message"><br><input type="submit" value="Post"></form> '
+	var commentForm = commentsString + '<form action=""> Feel free to drop a comment: <br> <input type="text" name="message"><br><input type="submit" value="Post"></form> '
 	var htmlString = 'Number of visitors to this site: ' + timesVisited + '<br>' + '<iframe src="//giphy.com/embed/l46CvHYDDsOocpRjq" width="480" height="268" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/colbertlateshow-the-late-show-with-stephen-colbert-l46CvHYDDsOocpRjq">via GIPHY</a></p>' + commentForm
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end(htmlString);
